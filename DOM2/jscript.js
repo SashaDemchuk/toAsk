e45a4d71ff17bc5daea27b0ec0be19ffb884fc5f
SashaDemchuk <shounboud@gmail.com>
2022-05-11 22:11:26 +0300

    why it does not work

---
+++ DOM2/jscript.js
@@ -0,0 +1,18 @@
+const bodyTextChange = 
+    <main>
+        <header class="header"><h1>This is completely new cite!</h1></header>
+        <nav>
+            <ul>
+                <li><a href="#">Top</a></li>
+                <li><a href="#">Midle top</a></li>
+                <li><a href="#">Midle</a></li>
+                <li><a href="#">Midle bottom</a></li>
+                <li><a class="bottom" href="#">Bottom</a></li>            
+            </ul>
+        </nav>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis tempore quod aperiam delectus nostrum? Ea, cumque neque. Labore, necessitatibus non? Vero illo consectetur quaerat? Explicabo molestias recusandae cumque corporis ex.
+        </p>
+    </main>
+
+document.body.innerHTML = bodyTextChange;
