0fe9adeac24600cdf7b364b83c20f2ad6ba121ce
SashaDemchuk <shounboud@gmail.com>
2022-06-05 11:38:54 +0300

    сранi event listeners

---
+++ Event Listeners/script.js
@@ -0,0 +1,16 @@
+/**
+ * Practice: Play with event listeners
+ * - Use an event listener and CSS either inline or through an added class to draw a highlight around the entire grid when you hover over it with your mouse.
+ * - Add an event listener to each grid cell to highlight that cell when you hover your mouse over it.
+ * - Add an event listener to each grid cell to change its background color when it is clicked.
+ * - Add an event listener to a specific key on the keyboard to change the background color of the whole page - from dark to light and back again.
+ */
+const cell01 = document.querySelectorAll(".cell01");
+cell01.addEventListener("mousenter", () => {
+    cell01.style.outline = "6px solid red";
+    });
+// const gridContainer = document.querySelector(".grid");
+
+// gridContainer.addEventListener("mouseenter", () => {
+//   gridContainer.style.outline = "6px solid red";
+// });
